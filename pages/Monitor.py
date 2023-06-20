d0fd17ed16d581ed11c91c41ca3a659f9850dc00
msoleimanian <36849205+msoleimanian@users.noreply.github.com>
2023-06-20 16:32:13 +0800

    Update Monitor.py

--- pages/Monitor.py
+++ pages/Monitor.py
@@ -1,3 +1,4 @@
+import july as july
 import numpy as np
 import streamlit as st
 import pandas as pd
@@ -51,56 +52,44 @@ with tab1:
    st.header("Farm1")
    col1, col2 = st.columns([2,1])
 
-   col1.subheader("Live Image")
+   col1.subheader("Live farm video feed")
    col1.image("Farm1.jpeg", width=600)
 
    col2.subheader("Details")
-   col2.markdown(printCostumTitleAndContenth4('Plot: ' , 'Each row is a Plot. 4 Plot and 10 subplot') , unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Plot asn SubPlot' , 'No. Plot : 10 and No. Subplot: 10') , unsafe_allow_html=True)
    col2.markdown(printCostumTitleAndContenth4('Location' , 'UPM') , unsafe_allow_html=True)
-   #col2.markdown(printCostumTitleAndContenth4('Location: ' , '') , unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Growth Cycle:', 'state : Maturation'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Time Table', 'week : 6/8'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Estimate of the Harvest', '15-20 days'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Growth Cycle', 'Maturation'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Weekly schedule', ' growth period: 9 week '), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContentext('elapsed time:  7 week'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Time left until harvest', '15-20 days'), unsafe_allow_html=True)
 
    st.markdown(printCostumTitleAndContenth4('Average of the Nutrients' , '') , unsafe_allow_html=True)
    dataframe = data_upload('farm 1.csv')
    print(dataframe)
-   dataframe1 = dataframe[['pH','SoilMoisture','N','P','EC','Zn','S','K','Mg','Ca']]
-   st.write(dataframe1)
+   st.write(dataframe)
 
-   st.markdown(printCostumTitleAndContenth4('Prediction Average of the Nutrients' , 'Nutrient prediction has been done with 98% accuracy in 8th week') , unsafe_allow_html=True)
-   dataframe1 = dataframe[['PrepH','PreSoilMoisture','PreN','PreP','PreEC','PreZn','PreS','PreK','PreMg','PreCa']]
-   st.write(dataframe1)
+
 
 with tab2:
    st.header("Farm2")
    col1, col2 = st.columns([2, 1])
 
-   col1.subheader("Live Image")
+   col1.subheader("Live farm video feed")
    col1.image("Farm2.jpeg", width=340)
 
    col2.subheader("Details")
-   col2.markdown(printCostumTitleAndContenth4('Plot: ', 'Each row is a Plot. 4 Plot and 10 subplot'),
+   col2.markdown(printCostumTitleAndContenth4('Plot asn SubPlot', 'No. Plot : 10 and No. Subplot: 10'),
                  unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Location' , 'UPM') , unsafe_allow_html=True)
-
-   # col2.markdown(printCostumTitleAndContenth4('Location: ' , '') , unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Growth Cycle:', 'state : Maturation'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Time Table', 'week : 7/8'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Estimate of the Harvest', '10-6 days'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Location', 'UPM'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Growth Cycle', 'Maturation'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Weekly schedule', ' growth period: 8 week '), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContentext('elapsed time:  6 week'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Time left until harvest', '17-22 days'), unsafe_allow_html=True)
 
    st.markdown(printCostumTitleAndContenth4('Average of the Nutrients', ''), unsafe_allow_html=True)
    dataframe = data_upload('farm 1.csv')
    print(dataframe)
-   dataframe1 = dataframe[['pH', 'SoilMoisture', 'N', 'P', 'EC', 'Zn', 'S', 'K', 'Mg', 'Ca']]
-   st.write(dataframe1)
-
-   st.markdown(printCostumTitleAndContenth4('Prediction Average of the Nutrients',
-                                            'Nutrient prediction has been done with 98% accuracy in 8th week '),
-               unsafe_allow_html=True)
-   dataframe1 = dataframe[
-      ['PrepH', 'PreSoilMoisture', 'PreN', 'PreP', 'PreEC', 'PreZn', 'PreS', 'PreK', 'PreMg', 'PreCa']]
-   st.write(dataframe1)
+   st.write(dataframe)
 
 
 
@@ -108,61 +97,46 @@ with tab3:
    st.header("Farm3")
    col1, col2 = st.columns([2, 1])
 
-   col1.subheader("Live Image")
+   col1.subheader("Live farm video feed")
    col1.image("Farm3.jpeg", width=600)
 
    col2.subheader("Details")
-   col2.markdown(printCostumTitleAndContenth4('Plot: ', 'Each row is a Plot. 4 Plot and 10 subplot'),
-                 unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Location' , 'UPM') , unsafe_allow_html=True)
-   # col2.markdown(printCostumTitleAndContenth4('Location: ' , '') , unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Growth Cycle:', 'state : Maturation'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Time Table', 'week : 5/8'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Estimate of the Harvest', '30-33 days'), unsafe_allow_html=True)
 
-   st.markdown(printCostumTitleAndContenth4('Average of the Nutrients', ''), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Plot asn SubPlot', 'No. Plot : 10 and No. Subplot: 10'),
+                 unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Location', 'UPM'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Growth Cycle', 'Maturation'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Weekly schedule', ' growth period: 10 week '), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContentext('elapsed time:  7 week'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Time left until harvest', '20-25 days'), unsafe_allow_html=True)
+
    dataframe = data_upload('farm 1.csv')
    print(dataframe)
-   dataframe1 = dataframe[['pH', 'SoilMoisture', 'N', 'P', 'EC', 'Zn', 'S', 'K', 'Mg', 'Ca']]
-   st.write(dataframe1)
-
-   st.markdown(printCostumTitleAndContenth4('Prediction Average of the Nutrients',
-                                            'Nutrient prediction has been done with 98% accuracy in 8th week'),
-               unsafe_allow_html=True)
-   dataframe1 = dataframe[
-      ['PrepH', 'PreSoilMoisture', 'PreN', 'PreP', 'PreEC', 'PreZn', 'PreS', 'PreK', 'PreMg', 'PreCa']]
-   st.write(dataframe1)
-
-
+   st.write(dataframe)
 
 
 with tab4:
    st.header("Farm4")
    col1, col2 = st.columns([2, 1])
 
-   col1.subheader("Live Image")
+   col1.subheader("Live farm video feed")
    col1.image("Farm4.jpeg", width=600)
 
    col2.subheader("Details")
-   col2.markdown(printCostumTitleAndContenth4('Plot: ', 'Each row is a Plot. 4 Plot and 10 subplot'),
+
+   col2.markdown(printCostumTitleAndContenth4('Plot asn SubPlot', 'No. Plot : 10 and No. Subplot: 10'),
                  unsafe_allow_html=True)
-   # col2.markdown(printCostumTitleAndContenth4('Location: ' , '') , unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Growth Cycle:', 'state : Maturation'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Time Table', 'week : 3/8'), unsafe_allow_html=True)
-   col2.markdown(printCostumTitleAndContenth4('Estimate of the Harvest', '45-50 days'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Location', 'UPM'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Growth Cycle', 'Maturation'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Weekly schedule', ' growth period: 8 week '), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContentext('elapsed time:  3 week'), unsafe_allow_html=True)
+   col2.markdown(printCostumTitleAndContenth4('Time left until harvest', '45-50 days'), unsafe_allow_html=True)
+
 
    st.markdown(printCostumTitleAndContenth4('Average of the Nutrients', ''), unsafe_allow_html=True)
    dataframe = data_upload('farm 1.csv')
    print(dataframe)
-   dataframe1 = dataframe[['pH', 'SoilMoisture', 'N', 'P', 'EC', 'Zn', 'S', 'K', 'Mg', 'Ca']]
-   st.write(dataframe1)
-
-   st.markdown(printCostumTitleAndContenth4('Prediction Average of the Nutrients',
-                                            'Nutrient prediction has been done with 98% accuracy in 8th week'),
-               unsafe_allow_html=True)
-   dataframe1 = dataframe[
-      ['PrepH', 'PreSoilMoisture', 'PreN', 'PreP', 'PreEC', 'PreZn', 'PreS', 'PreK', 'PreMg', 'PreCa']]
-   st.write(dataframe1)
+   st.write(dataframe)
 
 
 
