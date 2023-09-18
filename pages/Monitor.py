56fbf0cab8e45192d0c9369e4a045c67a6831d94
mohsen <mohsen.soleimanian@yahoo.com>
2023-09-18 14:07:15 +0800

    Update

--- pages/Monitor.py
+++ pages/Monitor.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from statistics import mean
+import altair as alt
 
 from PIL import Image
 from matplotlib import pyplot as plt
@@ -143,6 +144,19 @@ with tab4:
    st.plotly_chart(fig)
 
    st.line_chart(dataframe , x='week')
+
+   source = pd.melt(dataframe, id_vars=['Prediction Yeild(Kg)', 'Actual Yeild(Kg)' , 'Goal Yeild(Kg)'])
+   chart = alt.Chart(source).mark_bar(strokeWidth=100).encode(
+      x=alt.X('variable:N', title="", scale=alt.Scale(paddingOuter=0.5)),
+      # paddingOuter - you can play with a space between 2 models
+      y='value:Q',
+      color='variable:N',
+      column=alt.Column('model:N', title="", spacing=0),
+      # spacing =0 removes space between columns, column for can and st
+   ).properties(width=300, height=300, ).configure_header(labelOrient='bottom').configure_view(
+      strokeOpacity=0)
+
+   st.altair_chart(chart)  # , use_container_width=True)
    st.markdown(printCostumTitleAndContenth4('Average of the Nutrients', ''), unsafe_allow_html=True)
    dataframe = data_upload('farm 1.csv')
    print(dataframe)
